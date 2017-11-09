126f1e62dfebf59a584be8c4de9603718da218ba
lihaoliang <hzlihaoliang@corp.netease.com>
2017-11-09 16:52:48 +0800

    增加iconfont 自定义icon字体库

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="g-doc">test
     <router-view></router-view>
+    <i class="icon-packup"></i>    
   </div> 
-  
 </template>
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  
+}
 </script>
 <style lang="scss">
+@import "../css/iconfont/iconfont.css";
 .g-doc {
   width: 1200px;
   margin: 0 auto;
