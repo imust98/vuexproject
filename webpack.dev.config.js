f987ade754132549221c3409048f337df1ba1d8a
lihaoliang <hzlihaoliang@corp.netease.com>
2017-11-14 17:08:57 +0800

    优化webpack开发以及打包配置

---
+++ webpack.dev.config.js
@@ -0,0 +1,53 @@
+"use strict";
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.config')
+var StylelintPlugin = require('stylelint-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
+module.exports = merge(baseWebpackConfig, {
+  // cheap-module-eval-source-map is faster for development
+  devtool: 'eval',
+  devServer: {
+    hot: true,
+    open: true,
+    openPage:'userManage',
+    port:8090,
+    inline: true,
+    historyApiFallback:true
+  },
+  plugins: [
+    new StylelintPlugin({
+      files: [ 
+        '**/*.vue',
+        '**/*.scss'
+      ]
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      minChunks: Infinity
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename:'index.html',
+      template:'index.html',
+      inject: true
+    }),
+    new FriendlyErrorsPlugin()
+  ]
+})
